0c242c064ad06dddea6e99432a408067a0b0a83c
Peter Ly <peter.ly1@hotmail.com>
2022-05-04 15:25:53 +0100

    Added more tests

--- api/tests/unit/controllers/users.spec.js
+++ api/tests/unit/controllers/users.spec.js
@@ -29,25 +29,36 @@ describe('Users controller', () => {
             await usersController.getAllUsers(null, mockRes);
             expect(mockStatus).toHaveBeenCalledWith(200);
             expect(mockJson).toHaveBeenCalledWith([{}, {}])
+        });
+    });
+
+
+        describe('createUsers', () => {
+            it('creates users with status 201', async () => {
+                let testUser = [{
+                    username: 'peterlydev',
+                    score: '10'
+                }]
+
+                jest.spyOn(db, 'query')
+                .mockResolvedValue({rows: []})
+                .mockResolvedValue({rows: []});
+
+                jest.spyOn(User, 'createUsers')
+                .mockResolvedValue(new User({testUser, user_id:1}))
+                const mockReq = {body: {score: '10'}}
+                await usersController.createUsers(mockReq, mockRes)
+                expect(mockStatus).toHaveBeenCalledWith(201);
+            });
+        });
+
+        describe('createUsers', () => {
+            test('it resolves with user on successful db query', async () => {
+                let testUser = { id: 1, username: 'peterlydev', score: '10' }
+                jest.spyOn(db, 'query')
+                    .mockResolvedValueOnce({rows: [ testUser] });
+                const result = await usersController.createUsers('peterlydev');
+                expect(result).toHaveBeenCalledWith(201);
         })
-    })
-
-    describe('createUsers', () => {
-        it('creates users with status 201', async () => {
-            let testUser = [{
-                username: 'peterlydev',
-                score: '10'
-            }]
-
-            jest.spyOn(db, 'query')
-            .mockResolvedValue({rows: []})
-            .mockResolvedValue({rows: []});
-
-            jest.spyOn(User, 'createUsers')
-            .mockResolvedValue(new User({testUser, user_id:1}))
-            const mockReq = {body: {score: '10'}}
-            await usersController.createUsers(mockReq, mockRes)
-            expect(mockStatus).toHaveBeenCalledWith(201);
-        })
-    })
+    });
 })
