e92905109ea92c5c3dbe50a869d7cd9fb64f0b9f
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-05-05 21:38:51 +0100

    harry potter theme added

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -12,12 +12,14 @@ export const Results = () => {
 
   return (
       <>
-      <h1>Results</h1>
-      <h3>Score: {score}</h3>
-      {score<10?<><h2>Better luck next time</h2><img src='https://st2.depositphotos.com/1967477/6346/v/600/depositphotos_63465181-stock-illustration-sad-smiley-emoticon-cartoon.jpg' alt='sad face'></img></>:<><h2>Nice going!</h2><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Mr._Smiley_Face.svg/2048px-Mr._Smiley_Face.svg.png' alt='happy face'></img></>}
-      <h5>Leaderboard</h5>
+      <h1 className="resultTitle">Results</h1>
+      <h3 id="scoreTitle">Score: {score}</h3>
+      {score<10?<><h2 id='status'>Better luck next time</h2><img src='https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji-480x480.png.webp' alt='sad face'></img></>:<><h2>Nice going!</h2><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Mr._Smiley_Face.svg/2048px-Mr._Smiley_Face.svg.png' alt='happy face'></img></>}
+      <h5 id='leaderboardTitle'>Leaderboard</h5>
+      <div id='resultLeaderbaord'>
       <Leaderboard/>
       <button onClick={() => navigate('/')}>Play again?</button>
+      </div>
       </>
     
   )
